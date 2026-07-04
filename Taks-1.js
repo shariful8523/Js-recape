37e96f9902248ad69ea48ab3c2ee55cfd6ceb44d
Md.Shariful Islam <mdsharifulislam8523@gmail.com>
2026-07-04 20:34:42 +0600

    added 2nd taks

--- Taks-1.js
+++ Taks-1.js
@@ -1,4 +1,13 @@
 
-console.log(" My name:  shariful Islam");
-console.log("My Income: 20000")
-console.log(" My expenses: 20000")
+// console.log(" My name:  shariful Islam");
+// console.log("My Income: 20000")
+// console.log(" My expenses: 20000")
+
+
+// Variable diclear 
+
+const name = " Md shariful islam ";
+const amount = " 20000";
+const expenses = " 20000";
+
+console.log( name, amount, expenses );
